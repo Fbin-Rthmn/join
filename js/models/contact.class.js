35586239667f04abd445b3ccb0989a9e1ec5948c
Fbin-Rthmn <rothmann.fabian@googlemail.com>
2024-03-10 12:19:06 +0100

    update-2024-03-10

--- js/models/contact.class.js
+++ js/models/contact.class.js
@@ -2,11 +2,21 @@ class Contact {
     firstName;
     lastName;
     eMail;
+    phone;
 
-    constructor(firstName, lastName, eMail) {
+    constructor(firstName, lastName, eMail, phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.eMail = eMail;
+        this.phone = phone;
         console.log('New Contact created! ' + firstName + " " + lastName);
+    }
+
+    call() {
+        window.location.href = 'tel:' + this.phone;
+    }
+
+    printFullName() {
+        console.log(`${this.firstName} ${this.lastName}`);
     }
 }
--- up.bat
+++ up.bat
@@ -1,3 +1,5 @@
+@REM im Terminal die Funktion mit "./up.bat" aufrufen
+
 git pull
 git add .
 git commit -m "%*"
